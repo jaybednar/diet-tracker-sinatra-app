5ffaf0c1bdf1591aa3f5f70f33f8b789df1c5a7d
MyMac <mymac@MyMacs-MacBook-Pro.local>
2018-04-17 13:17:47 -0700

    added food attribute specs

--- spec/models/02_food_spec.rb
+++ spec/models/02_food_spec.rb
@@ -1,1 +1,33 @@
 require_relative "../spec_helper"
+
+describe Food do 
+	before(:each) do 
+		@food = Food.create(name: "Chicken Breast", serving_size: "4 oz", protein: 26, carbs: 0, fat: 2)
+	end 
+
+	describe 'attributes' do 
+
+		it 'has a name' do 
+			expect(@food.name).to eq("Chicken Breast") 
+		end 
+
+		it 'has a serving_size' do 
+			expect(@food.serving_size).to eq("4 oz")
+		end 
+
+		it 'has a protein value' do 
+			expect(@food.protein).to eq(26)
+		end 
+
+		it 'has a protein value' do 
+			expect(@food.carbs).to eq(0)
+		end 
+
+		it 'has a protein value' do 
+			expect(@food.fat).to eq(2)
+		end 
+
+	end
+
+
+end  
