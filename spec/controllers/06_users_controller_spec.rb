a28f0dd3153164a5823bd7c752aa4402c62f2f87
MyMac <mymac@MyMacs-MacBook-Pro.local>
2018-04-18 10:04:51 -0700

    added /login spec tests

--- spec/controllers/06_users_controller_spec.rb
+++ spec/controllers/06_users_controller_spec.rb
@@ -28,5 +28,30 @@ describe UsersController do
 	 	end 
 	end 
 
+	describe '/login' do 
+		before(:each) do 
+			visit '/login' 
+		end 
 
+		it "responds with a 200 status code" do
+	    expect(page.status_code).to eq(200)
+	  end
+
+	  it 'has the title of Login' do 
+	  	expect(page.body).to include("<h1>Login</h1>")
+	  end
+
+	  it 'has a form to login' do 
+	  	expect(page.body).to include("<form")
+	  	expect(page.body).to include("</form>")
+	 	end 
+	 	
+	 	it "sends a post request to '/login'"	do 
+	 		fill_in 'username', with: 'jb'
+	 		fill_in 'password', with: 'jjj'
+	 		click_button 'login'
+	 		expect(page.current_path).to eq('/login')
+	 	end 
+
+	end 
 end 
