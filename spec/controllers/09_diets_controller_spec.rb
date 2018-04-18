cd44852ec165fab12938e3fc244da74623d54750
MyMac <mymac@MyMacs-MacBook-Pro.local>
2018-04-18 11:18:47 -0700

    added diets controller diets show page specs

--- spec/controllers/09_diets_controller_spec.rb
+++ spec/controllers/09_diets_controller_spec.rb
@@ -1,1 +1,33 @@
 require_relative "../spec_helper"
+
+describe DietsController do 
+
+	describe '/diets/:slug' do 
+		before(:each) do 
+			visit '/diets/new' 
+		end 
+
+		it "responds with a 200 status code" do
+	    expect(page.status_code).to eq(200)
+	  end
+
+	  it 'lists all meals currently in diet' do 
+	  	expect(page.body).to include("@diet.meals.each")
+	  end 
+
+	  it 'lists total macros and calories for the day'
+
+	  it 'has a form to create and add new meal'
+
+	  it 'has a button to edit current diet' do 
+	  	expect(page).to have_css('input#edit_diet')
+	  end 
+
+	  it 'has a button to delete current diet' do 
+	  	expect(page).to have_css('input#delete_diet')
+	  end 
+
+	  it 'has a button to return to user homepage' do 
+	  	expect(page).to have_css('input#user_homepage')
+	  end 
+	end 
