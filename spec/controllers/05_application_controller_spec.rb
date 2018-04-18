00b85d3b6cc39c5d88851f95d1c3cf37d67c9d9b
MyMac <mymac@MyMacs-MacBook-Pro.local>
2018-04-18 09:46:19 -0700

    added application_controller_spec tests

--- spec/controllers/05_application_controller_spec.rb
+++ spec/controllers/05_application_controller_spec.rb
@@ -8,6 +8,23 @@ describe ApplicationController do
   it "responds with a welcome message" do
     get '/'
     expect(last_response.status).to eq(200)
-    expect(last_response.body).to include("Welcome to the Sinatra Template!")
+    expect(last_response.body).to include("Welcome to the Diet Tracker App!")
   end
+
+  it 'has a button to create a new account' do 
+  	expect(page).to have_css('input#create_new_account')
+  end 
+
+  it 'redirects you to the New User form page' do 
+  	expect(page.current_path).to eq("/users/new")
+  end 
+
+  it 'has a button to login for existing users' do
+  	expect(page).to have_css('input#login')
+  end 
+
+  it 'redirects you to the login form page' do 
+  	expect(page.current_path).to eq("/login")
+  end 
+
 end
