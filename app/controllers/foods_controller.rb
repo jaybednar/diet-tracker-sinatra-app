6f6b2e87c6660b64f7091116da80f0ac1cc11471
MyMac <mymac@MyMacs-MacBook-Pro.local>
2018-04-18 19:10:52 -0700

    Done.

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -11,28 +11,28 @@ class FoodsController < ApplicationController
     erb :"/foods/new.html"
   end
 
-  # POST: /foods
-  post "/foods" do
-    redirect "/foods"
-  end
+  # # POST: /foods
+  # post "/foods" do
+  #   redirect "/foods"
+  # end
 
-  # GET: /foods/5
-  get "/foods/:id" do
-    erb :"/foods/show.html"
-  end
+  # # GET: /foods/5
+  # get "/foods/:id" do
+  #   erb :"/foods/show.html"
+  # end
 
-  # GET: /foods/5/edit
-  get "/foods/:id/edit" do
-    erb :"/foods/edit.html"
-  end
+  # # GET: /foods/5/edit
+  # get "/foods/:id/edit" do
+  #   erb :"/foods/edit.html"
+  # end
 
-  # PATCH: /foods/5
-  patch "/foods/:id" do
-    redirect "/foods/:id"
-  end
+  # # PATCH: /foods/5
+  # patch "/foods/:id" do
+  #   redirect "/foods/:id"
+  # end
 
-  # DELETE: /foods/5/delete
-  delete "/foods/:id/delete" do
-    redirect "/foods"
-  end
+  # # DELETE: /foods/5/delete
+  # delete "/foods/:id/delete" do
+  #   redirect "/foods"
+  # end
 end
