7d94d5239a85197472d0eee8fc4024ac53e3846f
MyMac <mymac@MyMacs-MacBook-Pro.local>
2018-04-18 19:10:26 -0700

    stubbed out POST /meals route to add new meal to diet

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,37 +1,46 @@
 class MealsController < ApplicationController
 
   # GET: /meals
-  get "/meals" do
-    erb :"/meals/index.html"
-  end
+  # get "/meals" do
+  #   erb :"/meals/index.html"
+  # end
 
   # GET: /meals/new
-  get "/meals/new" do
-    erb :"/meals/new.html"
-  end
+  # get "/meals/new" do
+  #   erb
+  # end
 
-  # POST: /meals
+  # # POST: /meals
   post "/meals" do
-    redirect "/meals"
+    @meal = Meal.create(name: params[:meal][:name])
+
+    params[:foods].each do |food_num, food_hash|
+      food = Food.find(food_hash[:food_id])
+      @meal.add_food(food, food_hash[:food_servings])
+    end 
+
+    @meal.calculate_macros
+    @diet = Diet.find(params[:diet_id])  #will need to add a hidden input to with name="diet_id" and value="<%=@diet.id%>"
+    redirect to "/diets/#{@diet.id}"
   end
 
-  # GET: /meals/5
-  get "/meals/:id" do
-    erb :"/meals/show.html"
-  end
+  # # GET: /meals/5
+  # get "/meals/:id" do
+  #   erb :"/meals/show.html"
+  # end
 
-  # GET: /meals/5/edit
-  get "/meals/:id/edit" do
-    erb :"/meals/edit.html"
-  end
+  # # GET: /meals/5/edit
+  # get "/meals/:id/edit" do
+  #   erb :"/meals/edit.html"
+  # end
 
-  # PATCH: /meals/5
-  patch "/meals/:id" do
-    redirect "/meals/:id"
-  end
+  # # PATCH: /meals/5
+  # patch "/meals/:id" do
+  #   redirect "/meals/:id"
+  # end
 
-  # DELETE: /meals/5/delete
-  delete "/meals/:id/delete" do
-    redirect "/meals"
-  end
+  # # DELETE: /meals/5/delete
+  # delete "/meals/:id/delete" do
+  #   redirect "/meals"
+  # end
 end
