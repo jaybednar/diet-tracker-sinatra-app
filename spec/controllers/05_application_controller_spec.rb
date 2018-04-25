e6a5bb0b0dcad89596ac3f7c6a9c570a8bbd1bb0
MyMac <mymac@MyMacs-MacBook-Pro.local>
2018-04-25 14:44:17 -0700

    updated application controller specs

--- spec/controllers/05_application_controller_spec.rb
+++ spec/controllers/05_application_controller_spec.rb
@@ -9,29 +9,16 @@ describe ApplicationController do
     visit '/'
   end 
 
+  it 'loads correctly' do 
+    get '/'
+    expect(last_response.status).to eq(200)
+  end 
+
   it "responds with a welcome message" do
     get '/'
     expect(last_response.status).to eq(200)
     expect(last_response.body).to include("Welcome to the Diet Tracker App!")
   end
 
-  it 'has a button to create a new account' do 
-  	expect(page).to have_css('input#new_account')
-   
-  end 
-
-  it 'redirects you to the New User form page' do 
-    click_button 'new_account'
-  	expect(page.current_path).to eq("/users/new")
-  end 
-
-  it 'has a button to login for existing users' do
-  	expect(page).to have_css('input#login')
-  end 
-
-  it 'redirects you to the login form page' do 
-    click_button 'login'
-  	expect(page.current_path).to eq("/session/new")
-  end 
 
 end
